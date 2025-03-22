d9592e04936b564d0ffb62acf93690efdea8dc11
BuiVanThanh178 <bui055965@gmail.com>
2025-03-22 15:44:19 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php require('./includes/Header.php')?>
-		   <img src="img/hinh-nen-hoa-huong-duong-thumbnail.jpg" style="z-index:-1;margin-top:-10px;border-radius: 0px 0px 10px 10px;object-fit: cover;" height="700px" width="1351px" id="target-section" />
-		   <div class="content" style="text-align: center;margin-top: -400px;">
-			<h1>Chào Mừng Bạn đã đến với website</h1>
-			<p>xin chào đây là 1 website được copy í tương :></p>
-			<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href='login.php'" >Đăng nhập</div></button>
-			<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href='register.php'" >Đăng kí</div></button>
-		</div>
-<?php require('./includes/Footer.php')?>
